372fd86aa8f67f65a222f3598232e9a5c12e4bb3
donnyramad <donnyramad@gmail.com>
2026-05-25 14:16:21 +0800

    Create basic_data_analysis.py

---
+++ basic_data_analysis.py
@@ -0,0 +1,31 @@
+import pandas as pd
+
+# 1. Social Media Comments Data Simulation
+data = {
+    'username': ['user1', 'user2', 'user3', 'user4'],
+    'comment': [
+        'This game is great!', 
+        'I hate the latest update, bugs everywhere.', 
+        'is there any tips to get to level 5?', 
+        'boring gameplay and environment.'
+    ]
+}
+
+df = pd.DataFrame(data)
+
+# 2. Simple Function to Classified the Sentiments
+def sentiment_classification(text):
+    text = text.lower()
+    if 'great' in text or 'good' in text:
+        return 'Positive'
+    elif 'hate' in text or 'bug' in text or 'boring' in text:
+        return 'Negative'
+    else:
+        return 'Neutral'
+
+# 3. Applying the Classification
+df['sentiment'] = df['comment'].apply(sentiment_classification)
+
+# 4. Showing Result
+print("Comment Analysis Data Result :")
+print(df)
